9ff180ac696d947a98e0d7cbc0c8ccd0804dd463
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-11-27 10:42:18 -0700

    add knex file

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = process.env.PORT || 3500;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = process.env.PORT || 3100;
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors()); 
 
-app.get('/', (req, res) => {
-  res.json("ayyy")
+app.get("/", (req, res, next) => {
+  res.send("This works")
 });
 
 app.use(notFound);
 app.use(errorHandler);
 
 function notFound(err, req, res, next) {
-  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+  res.status(404).send({error: "Not found!", status: 404, url: req.originalUrl})
 };
 
 function errorHandler(err, req, res, next) {
-  console.error('Error', err)
-  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  console.error("Error", err)
+  const stack =  process.env.NODE_ENV !== "production" ? err.stack : undefined
   res.status(500).send({error: err.message, stack, url: req.originalUrl})
 };
 
