0126567370c4e4b200d7d537d967f63432d125e3
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-11-28 10:43:46 -0700

    add routes for users

--- routes/passport_users.js
+++ routes/passport_users.js
@@ -2,5 +2,23 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../db/connection");
 
+function getUsers(){
+	return knex('passport_users')
+		.select('*')
+}
+
+function getDestinationsForUser(user){
+	return knex('destination')
+		.select(knex.raw('destination.id as destination_id, destination.date, destination.distance, destination.difficulty, destination.notes'))
+		.innerJoin('passport_users', 'passport_users.id', 'destination.passport_users_id')
+		.whereIn('destination.passport_users_id', [user.id])
+}
+
+function getVisitedForUser(user){
+	return knex('visited')
+		.select(knex.raw('visited.id as visited_id, visited.date, visited.distance, visited.difficulty, visited.notes'))
+		.innerJoin('passport_users', 'passport_users.id', 'visited.passport_users_id')
+		.whereIn('visited.passport_users_id', [user.id])
+}
 
 module.exports = router
